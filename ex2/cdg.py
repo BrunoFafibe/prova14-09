1a335b7d4aa1a368192e7afa72969c38492253f0
BrunoFafibe <88518242+BrunoFafibe@users.noreply.github.com>
2021-09-14 22:55:26 -0300

    Update cdg.py

--- ex2/cdg.py
+++ ex2/cdg.py
@@ -1,1 +1,20 @@
 
+def soma(x, y):
+    k = 4
+    z = x + y
+    return z
+
+def produto(x, y):
+    k = 4
+    z = x * y
+    return z
+
+a = 2
+b = 3
+c = 4
+
+op1 = soma(c, b)
+op2 = produto(b, b)
+total = soma(op1, op2)
+print(op1, op2)
+print(total)
